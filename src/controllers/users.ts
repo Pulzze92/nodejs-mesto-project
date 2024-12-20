ca7ed9383df19766f4070eb36f884df2eeb11cac
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2024-12-21 00:46:48 +0100

    created users controller

---
+++ src/controllers/users.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { User } from '../models';
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка при получении пользователей' });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById({ id: req.params.userId });
+
+    if (!user) return res.status(404).send({ message: 'Пользователь с таким id не был найден' });
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, about, avatar } = req.body;
+    const user = await User.create({ name, about, avatar });
+
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(500).send({ message: 'Неверные данные' });
+  }
+};
