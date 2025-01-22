45080512d727ac17275a6e17bcb32b339564e3c6
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2025-01-22 13:42:02 +0100

    fixes

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,35 @@
-import express, { Response, NextFunction, Request } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import { errors } from 'celebrate';
+import { login, createUser } from './controllers/users';
+import { validateUserBody, validateAuthentication } from './validators/users';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
-import { CustomRequest } from './types';
-import STATUS_CODES from './utils/constants';
-import auth from './middlewares/auth';
-import cookieParser from 'cookie-parser';
-import { logRequest, logError } from './middlewares/logger';
 import { errorHandler } from './middlewares/error-handler';
-import { errors } from 'celebrate';
-import { NotFoundError } from './errors';
+import { logRequest, logError } from './middlewares/logger';
+import { limiter } from './middlewares/rateLimiter';
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(logRequest);
 
+app.use(limiter);
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.post('/signup', validateUserBody, createUser);
+app.post('/signin', validateAuthentication, login);
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError('Запрашиваемый ресурс не найден'));
-});
-
 app.use(logError);
-
 app.use(errors());
 app.use(errorHandler);
-
-app.use(auth);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,25 +2,18 @@ import { Router } from 'express';
 import {
   getUsers,
   getUserById,
-  createUser,
   updateProfile,
   updateAvatar,
-  login,
   getCurrentUser,
 } from '../controllers/users';
 import auth from '../middlewares/auth';
 import {
-  validateUserBody,
-  validateAuthentication,
   validateUserId,
   validateUserUpdate,
   validateAvatar,
 } from '../validators/users';
 
 const router = Router();
-
-router.post('/signin', validateAuthentication, login);
-router.post('/signup', validateUserBody, createUser);
 
 router.get('/', auth, getUsers);
 router.get('/me', auth, getCurrentUser);
